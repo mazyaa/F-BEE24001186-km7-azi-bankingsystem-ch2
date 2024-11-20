7d062d4b44e582ecfcd5eada1cf70697b5d9bcb5
AziSudarya1 <azisudarya98@gmail.com>
2024-11-20 13:56:00 +0700

    update sentry

--- instrument.js
+++ instrument.js
@@ -1,12 +1,8 @@
 // Import with `import * as Sentry from "@sentry/node"` if you are using ESM
 const Sentry = require("@sentry/node");
-const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
-  integrations: [
-    nodeProfilingIntegration(),
-  ],
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
 });
